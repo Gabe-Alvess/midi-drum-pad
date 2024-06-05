ff8efe93ae91f3c6120d29099644002303e4f608
Gabriel Alves <gabriel_vieira2018@hotmail.com>
2024-06-05 16:31:53 +0200

    Exercises

--- main.js
+++ main.js
@@ -13,3 +13,41 @@ for (let i = 0; i < keyList.length; i++) {
     playKeySound(keyId);
   };
 }
+
+// Exercise 1
+
+// const pomKey = document.querySelector(".pom_key");
+
+// let counter = 0;
+
+// function incrementCounter() {
+//   counter = counter + 1;
+//   console.log("Counter: " + counter);
+// }
+
+// pomKey.onclick = incrementCounter;
+
+// Exercise 2
+
+// const keyList = document.querySelectorAll(".key");
+// console.log(keyList);
+
+// Exercise 3
+
+// const keyList = document.querySelectorAll(".key");
+// keyList[3].onclick = incrementCounter
+
+// Exercise 4
+
+// let i = 0;
+
+// while (i < keyList.length) {
+//   keyList[i].onclick = incrementCounter;
+//   i++;
+// }
+
+// Exercise 5
+
+// for (let i = 0; i < keyList.length; i++) {
+//   keyList[i].onclick = incrementCounter;
+// }
